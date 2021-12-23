33f904494715cdfdffb27e223eba944995f167b1
Marymagdalene <chemtai.langat92@gmail.com>
2021-12-22 20:44:20 -0500

    listFiltering

---
+++ listFiltering.js
@@ -0,0 +1,12 @@
+function filtering(arr){
+  let newArr = []
+for(let i=0; i<arr.length; i++){
+  if(typeof(arr[i]) === "number"){
+    newArr.push(arr[i])
+  }else {
+    
+  }
+
+}
+return newArr
+}
